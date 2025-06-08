3bcce67a8540b9225005a5b82b32020159229c09
Alessian Dormal <alessian@alessian.be>
2025-06-08 10:49:00 +0200

    Fix spotless for naqline merge and add more content (#11)
    added naquadah solutions for every type of naquadah
    
    Co-authored-by: BattleGuy03 <battleguy2020@gmail.com>

--- src/main/java/com/hordeofgreg/hogilabs/api/material/LabsFirstDegreeMaterials.java
+++ src/main/java/com/hordeofgreg/hogilabs/api/material/LabsFirstDegreeMaterials.java
@@ -32,28 +32,33 @@ public class LabsFirstDegreeMaterials {
                 .components(Molybdenum, 1, Sulfur, 2)
                 .build();
 
-        NaquadahLight = new Material.Builder(9002, labsId("light_naquadah"))
+        NaquadahRaw = new Material.Builder(9002, labsId("naquadah_raw"))
+                .dust()
+                .color(0x6a6c70).iconSet(MaterialIconSet.METALLIC)
+                .build();
+
+        NaquadahLight = new Material.Builder(9003, labsId("naquadah_light"))
                 .ingot().fluid().dust()
                 .color(0x3b3c3d).iconSet(MaterialIconSet.METALLIC)
-                .flags(GENERATE_FOIL, GENERATE_PLATE, GENERATE_DOUBLE_PLATE, GENERATE_DENSE, GENERATE_ROTOR, GENERATE_ROD, GENERATE_FRAME, GENERATE_GEAR, GENERATE_SMALL_GEAR)
+                .flags(GENERATE_PLATE)
                 .blast(builder -> builder
                         .temp(5300, BlastProperty.GasTier.HIGH)
                         .blastStats(VA[LuV], 45 * 20))
                 .build();
 
-        NaquadahHeavy = new Material.Builder(9003, labsId("heavy_naquadah"))
+        NaquadahHeavy = new Material.Builder(9004, labsId("naquadah_heavy"))
                 .ingot().fluid().dust()
                 .color(0x1d1e20).iconSet(MaterialIconSet.METALLIC)
-                .flags(GENERATE_FOIL, GENERATE_PLATE, GENERATE_DOUBLE_PLATE, GENERATE_DENSE, GENERATE_ROTOR, GENERATE_ROD, GENERATE_FRAME, GENERATE_GEAR, GENERATE_SMALL_GEAR)
+                .flags(GENERATE_PLATE)
                 .blast(builder -> builder
                         .temp(6300, BlastProperty.GasTier.HIGH)
                         .blastStats(VA[UV], 50 * 20))
                 .build();
 
-        NaquadahSuperheavy = new Material.Builder(9004, labsId("superheavy_naquadah"))
+        NaquadahSuperheavy = new Material.Builder(9005, labsId("naquadah_superheavy"))
                 .ingot().fluid().dust()
                 .color(0x050506).iconSet(MaterialIconSet.METALLIC)
-                .flags(GENERATE_FOIL, GENERATE_PLATE, GENERATE_DOUBLE_PLATE, GENERATE_DENSE, GENERATE_ROTOR, GENERATE_ROD, GENERATE_FRAME, GENERATE_GEAR, GENERATE_SMALL_GEAR)
+                .flags(GENERATE_PLATE)
                 .blast(builder -> builder
                         .temp(6900, BlastProperty.GasTier.HIGHER)
                         .blastStats(VA[UEV], 60 * 20))
